9f16b38b1fd829de4af04c082de86712c4eb9d4e
lichi <lichi.in>
2023-12-04 00:57:38 +0530

    seventh change

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 
 import Home from './pages/home/Home';
-import Order from './pages/order/Order';
+import Order from './pages/Order/Order';
 import Cart from './pages/cart/Cart';
 import Dashboard from './pages/admin/dashboard/Dashboard';
 import NoPage from './pages/nopage/NoPage';
